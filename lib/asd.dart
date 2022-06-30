9f2f7fc4507cbb56055a6ca7a85a0a3373c2d2f3
amrain <abd.3mrain@gmail.com>
2022-07-01 00:34:37 +0300

    tow

---
+++ lib/asd.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class ASd extends StatefulWidget {
+  const ASd({Key? key}) : super(key: key);
+
+  @override
+  _ASdState createState() => _ASdState();
+}
+
+class _ASdState extends State<ASd> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
